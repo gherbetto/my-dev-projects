7adcb74cb2e45b00c17348b7982f77b796a29e41
Dmitrii Gherastovschi <dgherastovschi@gmail.com>
2024-10-04 16:35:28 +0500

    finished details section with modal and a navbar color change on user scroll

--- bootstrap5/ebook-website/js/script.js
+++ bootstrap5/ebook-website/js/script.js
@@ -1,1 +1,13 @@
-// Add custom JavaScript here
+function userScrool() {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('bg-dark', 'navbar-sticky');
+        } else {
+            navbar.classList.remove('bg-dark', 'navbar-sticky');
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', userScrool);
